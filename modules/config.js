70102e6e66d49a9149229053945688b057516901
Sparky-web <b.abinovvlad@gmail.com>
2020-07-22 11:19:54 +0300

    update cookies

--- modules/config.js
+++ modules/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Mobile Safari/537.36",
-    "cookie": "tz=Europe%2FMoscow; __cfduid=daed15e2c7d3f39024d30c5173fb942021593509705; sid=bstvbsjasqjkm0cl5v8t82u86h; _ga=GA1.2.1907179290.1593509706; tz=Europe%2FMoscow; _gid=GA1.2.841383963.1595232100; _gat=1"
+    "cookie": "__cfduid=daed15e2c7d3f39024d30c5173fb942021593509705; sid=bstvbsjasqjkm0cl5v8t82u86h; _ga=GA1.2.1907179290.1593509706; _gid=GA1.2.1427430851.1595405683"
 }
